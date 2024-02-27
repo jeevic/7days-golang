2b6ab34aa33d60ef473d3337b405a1fe0418126c
jeevi cao <542561541@qq.com>
2024-02-27 23:11:36 +0800

    Add: context

--- gee-web/gee/context.go
+++ gee-web/gee/context.go
@@ -1,1 +1,30 @@
 package gee
+
+import "net/http"
+
+type H map[string]interface{}
+
+type Context struct {
+	Writer http.ResponseWriter
+	Req    *http.Request
+
+	//request info
+	Path   string
+	Method string
+
+	//response code
+	StatusCode string
+}
+
+func newContext(w http.ResponseWriter, req *http.Request) *Context {
+	return &Context{
+		Writer: w,
+		Req:    req,
+		Path:   req.URL.Path,
+		Method: req.Method,
+	}
+}
+
+func (c *Context) PostForm(key string) string {
+	return c.Req.FormValue(key)
+}
